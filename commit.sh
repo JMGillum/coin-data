0dba4374a5605d50adfd77ff5c874eb037fbd873
JMGillum <JMGillum@protonmail.com>
2026-02-21 20:14:07 -0800

    Removed unused script

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat ./test_setup_countries.sql >> ./setup_countries.sql
-cat ./test_setup_denominations.sql >> ./setup_denominations.sql
-cat ./test_setup_values.sql >> ./setup_values.sql
-cat ./test_setup_coins.sql >> ./setup_coins.sql
-cat ./test_setup_countries.sql >> ./setup_coins_years.sql
-
-rm ./test_setup_*.sql
