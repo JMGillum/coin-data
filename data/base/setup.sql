2734b3c0427e117033f5096d3d7dd523b377b744
JMGillum <JMGillum@protonmail.com>
2026-03-05 20:29:34 -0800

    Changed data organization structure.

---
+++ data/base/setup.sql
@@ -0,0 +1,118 @@
+CREATE TABLE version (
+  major INT(11),
+  minor INT(11),
+  PRIMARY KEY (major,minor)
+);
+
+INSERT INTO version(major,minor) VALUES(1,0);
+
+CREATE TABLE metals (
+  metal_id varchar(5) PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  price decimal(30,10) NOT NULL,
+  price_date date NOT NULL,
+  series varchar(32) NOT NULL
+);
+-- Metal types
+insert into metals(metal_id,name,price,price_date,series) values("au","gold",-1,"1000-01-01","base"),("ag","silver",-1,"1000-01-01","base"),("pd","palladium",-1,"1000-01-01","base"),("pt","platinum",-1,"1000-01-01","base"),("rh","rhodium",-1,"1000-01-01","base"),("other","other",-1,"1000-01-01","base");
+
+CREATE TABLE tags (
+  tag_id varchar(255) PRIMARY KEY,
+  bullion boolean DEFAULT FALSE
+);
+-- Tags
+INSERT INTO tags(tag_id) VALUES("none");
+INSERT INTO tags(tag_id,bullion) VALUES("bullion",TRUE);
+
+CREATE TABLE countries (
+  country_id varchar(255) PRIMARY KEY,
+  display_name varchar(255) NOT NULL,
+  tags varchar(255) default "none",
+  FOREIGN KEY (tags) REFERENCES tags(tag_id) ON UPDATE CASCADE,
+  series varchar(32) NOT NULL
+);
+
+CREATE TABLE country_names (
+  name varchar(255) PRIMARY KEY,
+  country_id varchar(255) NOT NULL,
+  FOREIGN KEY (country_id) REFERENCES countries(country_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE denominations (
+  denomination_id varchar(255) PRIMARY KEY,
+  country_id varchar(255) NOT NULL,
+  FOREIGN KEY (country_id) REFERENCES countries(country_id) ON UPDATE CASCADE,
+  display_name varchar(255) NOT NULL,
+  tags varchar(255) DEFAULT "none",
+  FOREIGN KEY (tags) REFERENCES tags(tag_id) ON UPDATE CASCADE,
+  series varchar(32) NOT NULL
+);
+
+CREATE TABLE denomination_names (
+  name_id INT AUTO_INCREMENT PRIMARY KEY,
+  denomination_id varchar(255) NOT NULL,
+  FOREIGN KEY (denomination_id) REFERENCES denominations(denomination_id) ON UPDATE CASCADE,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE face_values (
+  value_id varchar(255) PRIMARY KEY,
+  denomination_id varchar(255) NOT NULL,
+  FOREIGN KEY (denomination_id) REFERENCES denominations(denomination_id) ON UPDATE CASCADE,
+  value decimal(20,10) NOT NULL,
+  display_name varchar(255),
+  tags varchar(255) DEFAULT "none",
+  FOREIGN KEY (tags) REFERENCES tags(tag_id) ON UPDATE CASCADE,
+  series varchar(32) NOT NULL
+);
+
+CREATE TABLE face_values_names (
+  name_id INT AUTO_INCREMENT PRIMARY KEY,
+  value_id varchar(255) NOT NULL,
+  FOREIGN KEY (value_id) REFERENCES face_values(value_id) ON UPDATE CASCADE,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE coins (
+  coin_id varchar(255) PRIMARY KEY,
+  face_value_id varchar(255) NOT NULL,
+  FOREIGN KEY (face_value_id) REFERENCES face_values(value_id) ON UPDATE CASCADE,
+  gross_weight decimal(20,10) NOT NULL,
+  fineness decimal(11,10) NOT NULL,
+  precious_metal_weight decimal(20,10),
+  tags varchar(255) DEFAULT "none",
+  FOREIGN KEY (tags) REFERENCES tags(tag_id) ON UPDATE CASCADE,
+  metal varchar(5) NOT NULL,
+  FOREIGN KEY (metal) REFERENCES metals(metal_id) ON UPDATE CASCADE,
+  name varchar(255),
+  series varchar(32) NOT NULL
+);
+
+CREATE TABLE years (
+  coin_id varchar(255) NOT NULL,
+  year int NOT NULL,
+  FOREIGN KEY (coin_id) REFERENCES coins(coin_id) ON UPDATE CASCADE,
+  tags varchar(255) DEFAULT "none",
+  FOREIGN KEY (tags) REFERENCES tags(tag_id) ON UPDATE CASCADE,
+  PRIMARY KEY(coin_id,year)
+);
+
+CREATE TABLE specific_coins (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  coin_id varchar(255) NOT NULL,
+  FOREIGN KEY (coin_id) REFERENCES coins(coin_id) ON UPDATE CASCADE,
+  year int,
+  mintmark varchar(255)
+);
+
+CREATE TABLE purchases (
+  purchase_id int PRIMARY KEY AUTO_INCREMENT,
+  coin_id varchar(255) NOT NULL,
+  FOREIGN KEY (coin_id) REFERENCES coins(coin_id) ON UPDATE CASCADE,
+  purchase_date date NOT NULL,
+  unit_price decimal(30,10) NOT NULL,
+  purchase_quantity int DEFAULT 1,
+  specific_coin int,
+  FOREIGN KEY (specific_coin) REFERENCES specific_coins(id) ON UPDATE CASCADE
+);
+
---
+++ data/bullion/denominations.sql
@@ -0,0 +1,14 @@
+INSERT INTO denominations(denomination_id, country_id, display_name, tags, series) VALUES
+('can_maple', 'can', 'maple', 'bullion', 'base'),
+('gbr_britannia', 'gbr', 'britannia', 'bullion', 'base'),
+('usa_bullion', 'usa', 'bullion', 'bullion', 'base'),
+('zaf_krugerrand', 'zaf', 'krugerrand', 'bullion', 'base');
+INSERT INTO denomination_names(denomination_id, name) VALUES
+('can_maple', 'maple'),
+('can_maple', 'maples'),
+('gbr_britannia', 'britannia'),
+('gbr_britannia', 'britannias'),
+('usa_bullion', 'bullion'),
+('usa_bullion', 'bullions'),
+('zaf_krugerrand', 'krugerrand'),
+('zaf_krugerrand', 'krugerrands');
