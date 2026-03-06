bf1b930dc834b62fbf0f1a1bfeea99303e587c76
JMGillum <JMGillum@protonmail.com>
2026-03-05 23:52:46 -0800

    Added mexican libertads

--- data/bullion/denominations.sql
+++ data/bullion/denominations.sql
@@ -1,6 +1,7 @@
 INSERT INTO denominations(denomination_id, country_id, display_name, tags, series) VALUES
 ('can_maple', 'can', 'maple', 'bullion', 'bullion'),
 ('gbr_britannia', 'gbr', 'britannia', 'bullion', 'bullion'),
+('mex_libertad', 'mex', 'libertad', 'bullion', 'bullion'),
 ('usa_bullion', 'usa', 'bullion', 'bullion', 'bullion'),
 ('zaf_krugerrand', 'zaf', 'krugerrand', 'bullion', 'bullion');
 INSERT INTO denomination_names(denomination_id, name) VALUES
@@ -8,6 +9,8 @@ INSERT INTO denomination_names(denomination_id, name) VALUES
 ('can_maple', 'maples'),
 ('gbr_britannia', 'britannia'),
 ('gbr_britannia', 'britannias'),
+('mex_libertad', 'libertad'),
+('mex_libertad', 'libertads'),
 ('usa_bullion', 'bullion'),
 ('usa_bullion', 'bullions'),
 ('zaf_krugerrand', 'krugerrand'),
